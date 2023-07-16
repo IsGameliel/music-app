eb302e125b5c853bb377d1b4cb6024855318658b
isgameliel <simvicpro@gmail.com>
2023-07-16 09:55:10 +0100

    ui fix

--- configfiles/artists.py
+++
@@ -1,6 +0,0 @@
-def spotify_artists():
-    artists = {'Kanye West': 'spotify:artist:5K4W6rqBFWDnAN6FQUkS6x',
-               'Drake': 'spotify:artist:3TVXtAsR1Inumwj472S9r4',
-               'Kota The Friend': 'spotify:artist:2AfU5LYBVCiCtuCCfM7uVX',
-               'Denzel Curry': 'spotify:artist:6fxyWrfmjcbj5d12gXeiNV'}
-    return artists
